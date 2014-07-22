605315466f65abe8ff6bb5de993af50cafdaaf2b
andreij6 <andreij6@gmail.com>
2014-07-22 02:00:23 -0500

    $apply to update angular

--- Spatial.Web/Scripts/SignalR/srDemo.js
+++ Spatial.Web/Scripts/SignalR/srDemo.js
@@ -5,27 +5,28 @@
 myApp = angular.module('myApp', []);
 
 myApp.controller('ChatCtrl', function ($scope) {
-    $scope.Hello = "Hi";
     $scope.Message = "";
     $scope.Messages = [];
 
     var Hub = $.connection.spatialHub;
 
-        $.connection.hub.logging = true;
+    //$.connection.hub.logging = true;
 
-        $.connection.hub.start();
+    $.connection.hub.start();
 
-        Hub.client.newMessage = function (message) {
-            console.log(message);
-        };
-
-        $scope.sendMessage = function () {
-            Hub.server.send($scope.Message);
-            $scope.Message = "";
-        }
-        
-        $scope.addMessage = function (message) {
+    Hub.client.newMessage = function (message) {
+        $scope.$apply(function (){
             $scope.Messages.push(message);
-        }
+        })
+    }
+
+    $scope.sendMessage = function () {
+        Hub.server.send($scope.Message);
+        $scope.Message = "";
+    }
+        
+    //$scope.addMessage = function (message) {
+    //    $scope.Messages.push(message);
+    //}
 
 });
